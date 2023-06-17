27c9d1d5c9693efe3a894f2024af726af53bb935
Alessandro1918 <alessandro1918@hotmail.com>
2023-06-17 01:35:51 -0300

    feat: add p2p video connection with 'simple-peer' lib

--- src/pages/room/[roomId]/index.tsx
+++ src/pages/room/[roomId]/index.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from 'react'
 import { useRouter } from 'next/router'
+import Peer from "simple-peer";
 
 // import styles from '@/styles/Home.module.css'
 
@@ -7,15 +8,23 @@ import { socket } from '../../../socket'
 
 export default function Room() {
 
+  interface PeerRefProps {
+    peerId: string
+    peer: Peer.Instance
+  }
+
   const router = useRouter()                                  //get roomId from url
 
   const [ roomId, setRoomId ] = useState<any>(undefined)      //init var on the state to avoid multiple reloads
-  const [ userId, setUserId ] = useState('')
+  const [ myUserId, setMyUserId ] = useState('')
   const [ isVideoOn, setIsVideoOn ] = useState(true) 
   const [ isAudioOn, setIsAudioOn ] = useState(true) 
 
   const myStreamRef = useRef<HTMLVideoElement>(null)
   // const [ stream, setStream ] = useState<MediaStream>()
+
+  const peersRefs = useRef<PeerRefProps[]>([])                //ties the "Peer" object with it's socket.id
+  const [ peers, setPeers ] = useState<Peer.Instance[]>([])   //map thru this array to get the users videos
 
   useEffect(() => {
     
@@ -23,20 +32,38 @@ export default function Room() {
 
     setRoomId(router.query.roomId)
 
-    setupStream(isVideoOn, isAudioOn)
+    navigator.mediaDevices.getUserMedia({
+      video: isVideoOn,
+      audio: isAudioOn
+    }).then(currentStream => {
+      myStreamRef.current!.srcObject = currentStream
+      // setStream(currentStream)
 
-    socket.connect()
-    socket.on("connect", () => {
-      setUserId(socket.id)
-      socket.emit("join-room", router.query.roomId)
+      socket.connect()
+      socket.on("connect", () => {
+        setMyUserId(socket.id)
+
+        socket.on('list-room-users', users =>  {
+          console.log(`Users already in the room: ${users}`)
+          createUsers(users, socket.id, currentStream)
+        })
+
+        socket.on('new-signal-available', payload =>  {
+          console.log(`Got signal from new user in the room: user ${payload.callerId}`)
+          addUser(payload.signal, payload.callerId, currentStream)
+        })
+
+        socket.on('return-signal-available', payload =>  {
+          console.log(`Got signal from user already in the room: user ${payload.id}`)
+          linkPeerToUser(payload.signal, payload.id)
+        })
+
+        socket.emit("join-room", router.query.roomId)
+      })
     })
 
     socket.on('new-user-joined-room', userId =>  {
       console.log(`User ${userId} entered the room`)
-    })
-
-    socket.on('list-room-users', users =>  {
-      console.log(`Users already in the room: ${users}`)
     })
 
     socket.on('new-message-to-room', (userId, message) => {
@@ -45,44 +72,116 @@ export default function Room() {
 
     socket.on('left-room', userId => {
       console.log(`User ${userId} left the room`)
+      removeUser(userId)
     })
 
     return () => {
       //Clean up:
       socket.off('new-user-joined-room')
-      socket.off('list-room-users')
       socket.off('new-message-to-room')
       socket.off('left-room')
       socket.disconnect()
     }
   }, [router.isReady])
 
-  //Update stream each time the user change their audio/video configs
-  useEffect(() => {
-    setupStream(isVideoOn, isAudioOn)
-  }, [isVideoOn, isAudioOn])
+  //Saves every user already in the room as a peer on my state/ref and send them my id/stream
+  function createUsers(users: string[], myId: string, myStream: MediaStream) {
+    const peers: Peer.Instance[] = []
 
-  //Configure stream based on local vars (stream should include video? Audio?)
-  function setupStream(isVideoOn: boolean, isAudioOn: boolean) {
-    if (!isVideoOn && !isAudioOn) {
-      myStreamRef.current!.srcObject = null
-      // setStream(undefined)
-      return
-    }
-    navigator.mediaDevices.getUserMedia({
-      video: isVideoOn,
-      audio: isAudioOn
-    }).then(currentStream => {
-      myStreamRef.current!.srcObject = currentStream
-      //setStream(currentStream)
+    users.map(userId => {
+      const peer = createPeer(userId, myId, myStream)
+      peersRefs.current.push({
+        peerId: userId,
+        peer,
+      })
+      peers.push(peer)
     })
+
+    setPeers(peers)
+  }
+
+  //From my point of view (client), I`m starting the communication (initiator: true)
+  function createPeer(receiverId: string, callerId: string, stream: MediaStream) {
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream,
+    })
+
+    peer.on("signal", signal => {
+      socket.emit("send-signal", { receiverId, callerId, signal })
+    })
+
+    return peer
+  }
+
+  //Saves the new user in the room as a peer on my state/ref and send my id/stream
+  function addUser(signal: Peer.SignalData, callerId: string, stream: MediaStream) {
+    const peer = addPeer(signal, callerId, stream)
+
+    peersRefs.current.push({
+      peerId: callerId,
+      peer,
+    })
+
+    setPeers(users => [...users, peer])
+  }
+
+  //From my point of view (client), the other peer started the communication (initiator: false)
+  function addPeer(incomingSignal: Peer.SignalData, callerId: string, stream: MediaStream) {
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream,
+    })
+
+    peer.on("signal", signal => {
+      socket.emit("return-signal", { signal, callerId })
+    })
+
+    peer.signal(incomingSignal)
+
+    return peer
+  }
+
+  //Join to the users's ref stream the incoming peer's signal
+  function linkPeerToUser(signal: Peer.SignalData, id: string) {
+    const item = peersRefs.current.find(p => p.peerId === id)
+    item!.peer.signal(signal)
+  }
+
+  //Delete disconnected user from my UI
+  function removeUser(userId: string) {
+    // TODO
+  }
+
+  //Returns a video element of one peer
+  function PeerVideo(props: any) {
+    const ref = useRef<HTMLVideoElement>(null)
+  
+    useEffect(() => {
+      props.peer.on("stream", (stream: MediaStream) => {
+        ref.current!.srcObject = stream
+      })
+    }, [])
+  
+    return (
+      <div>
+        <video 
+          ref={ref} 
+          autoPlay
+          playsInline
+          style={{ width: '400px', height: '300px' }}
+        />
+      </div>
+    )
   }
 
   function sendMessageToRoom() {
     socket.emit(
       "send-message-to-room", 
       roomId, 
-      userId, 
+      myUserId, 
       `Room ${roomId} rules!`
     )
   }
@@ -91,7 +190,7 @@ export default function Room() {
     <>
       <main >
         <h1>{`Room: ${roomId}`}</h1>
-        <h3>{`User: ${userId}`}</h3>
+        <h3>{`User: ${myUserId}`}</h3>
         <input 
           type="button" 
           value="Send" 
@@ -101,6 +200,7 @@ export default function Room() {
           <video 
             ref={myStreamRef} 
             autoPlay 
+            playsInline
             style={{ width: '400px', height: '300px' }}
           />
         </div>
@@ -114,6 +214,17 @@ export default function Room() {
           value={isAudioOn ? "Audio: ON" : "Audio: OFF"} 
           onClick={() => setIsAudioOn(!isAudioOn)}
         />
+        {peers.map((peer, index) => {
+        {/* {peersRefs.current.map((ref, index) => { */}
+          return (
+            <div key={index}>
+              <PeerVideo key={index} peer={peer} />
+              {/* <PeerVideo key={index} peer={ref.peer} /> */}
+              <h3>{`User: ${peersRefs.current[index].peerId}`}</h3>
+              {/* <h3>{`User: ${ref.peerId}`}</h3> */}
+            </div>
+          );
+        })}
       </main>
     </>
   )
